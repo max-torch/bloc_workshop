5ecddb80dae703cd5c32d0e51349508488c6c6bd
Alexander Lacson <lacsonalexanderz@gmail.com>
2023-02-16 16:06:12 +0800

    Build based on the state from the two blocs in the second screen
    Show that state is preserved across screens

--- lib/presentation/screens/second_screen.dart
+++ lib/presentation/screens/second_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/first_boolean/first_boolean_bloc.dart';
+import '../../bloc/second_boolean/second_boolean_bloc.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({
@@ -17,7 +21,12 @@ class SecondScreen extends StatelessWidget {
             SizedBox(
               height: 20,
             ),
-            Text("BooleanA and BooleanB"),
+            Builder(builder: (context) {
+              final stateA = context.watch<FirstBooleanBloc>().state;
+              final stateB = context.watch<SecondBooleanBloc>().state;
+              return Text(
+                  "${stateA.toString().toUpperCase()} and ${stateB.toString().toUpperCase()}");
+            }),
             SizedBox(
               height: 20,
             ),
