637bcd8b0d7a9e9ca361d3428bc80c8cf9be21df
Alexander Lacson <lacsonalexanderz@gmail.com>
2023-02-16 14:38:15 +0800

    Declare true as initial value

--- lib/bloc/first_boolean_bloc.dart
+++ lib/bloc/first_boolean_bloc.dart
@@ -3,8 +3,8 @@ import 'package:meta/meta.dart';
 
 part 'first_boolean_event.dart';
 
-  FirstBooleanBloc() : super(FirstBooleanInitial()) {
 class FirstBooleanBloc extends Bloc<FirstBooleanEvent, bool> {
+  FirstBooleanBloc() : super(true) {
     on<FirstBooleanEvent>((event, emit) {
       // TODO: implement event handler
     });
