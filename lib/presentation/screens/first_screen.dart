fdd5c8651ab31675489c6340d05d9a9a6155168e
Alexander Lacson <lacsonalexanderz@gmail.com>
2023-02-16 13:44:34 +0800

    Create First Screen

---
+++ lib/presentation/screens/first_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
+    );
+  }
+}
