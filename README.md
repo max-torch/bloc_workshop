5fce017bd89a375b14da0b33224d82d8058b0b55
Alexander Lacson <lacsonalexanderz@gmail.com>
2023-02-16 13:22:35 +0800

    Run Flutter Create Application(Empty)

---
+++ README.md
@@ -0,0 +1,3 @@
+# bloc_workshop
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/bloc_workshop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bloc_workshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
