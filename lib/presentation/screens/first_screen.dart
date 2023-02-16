24a3320e3ad0865ff22ba62a904af5cf75851919
Alexander Lacson <lacsonalexanderz@gmail.com>
2023-02-16 13:54:12 +0800

    Add Buttons and Text of FirstScreen

--- lib/presentation/screens/first_screen.dart
+++ lib/presentation/screens/first_screen.dart
@@ -8,8 +8,32 @@ class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("First Screen")),
       body: Center(
-        child: Text('Hello World!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello World!'),
+            SizedBox(
+              height: 20,
+            ),
+            Text("BooleanA and BooleanB"),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                child: Text("Toggle First Boolean Bloc"), onPressed: () {}),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                child: Text("Toggle Second Boolean Bloc"), onPressed: () {}),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(child: Text("To Second Page"), onPressed: () {}),
+          ],
+        ),
       ),
     );
   }
